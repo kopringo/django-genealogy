cfcb9f99d2b341bc9732c2149af638dac9025c24
kopringo <kopringo@alpha-laptop>
2014-08-10 17:35:15 +0200

    testing setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='genealogy',
+      version='0.1',
+      description='Django genealogy app',
+      author='Michal Koperkiewicz',
+      author_email='gward@python.net',
+      url='http://github.com/kopringo/django-genealogy',
+      packages=['genealogy',],
+     )
